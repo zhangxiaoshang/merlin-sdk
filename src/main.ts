f60d2a4d4a6e8152d87577991229f18b8d715d17
zhangxiaoshang <zhangxiaoshang66@163.com>
2022-05-07 23:23:40 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,5 @@
-/**
- * Some predefined delay values (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
-}
-
-/**
- * Returns a Promise<string> that resolves after a given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - A number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
-}
-
-// Below are examples of using ESLint errors suppression
-// Here it is suppressing a missing return type definition for the greeter function.
-
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export async function greeter(name: string) {
-  return await delayedHello(name, Delays.Long);
+// 这里必须有一行注释，否则 test 会有语法异常
+// 可以删除这两行注释后执行 npm run test 查看异常情况
+export function sayHi(name: string) {
+  return `Hi ${name}`;
 }
